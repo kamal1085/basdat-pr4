85af901c78aea5e92a40295aa24288a6950c25b5
Zetra <zetra@mail.ugm.ac.id>
2017-05-06 17:18:40 +0700

    Handle create multiple sub category form

--- application/views/_templates/footer.php
+++ application/views/_templates/footer.php
@@ -10,39 +10,6 @@
 <!-- our JavaScript -->
 <script src="<?php echo URL; ?>public/js/jquery.min.js"></script>
 <script src="<?php echo URL; ?>public/js/bootstrap.min.js"></script>
-<script>
-$(document).ready(function() {
-    bookIndex = 0;
-
-    $('#tokoForm').on('click', '.addButton', function() {
-            bookIndex++;
-            var $template = $('#JKTemplate'),
-                $clone    = $template
-                                .clone()
-                                .removeClass('hide')
-                                .removeAttr('id')
-                                .attr('index', bookIndex)
-                                .insertBefore($template);
-
-            // Update the name attributes
-            $clone
-                .find('[name="toko_jk"]').attr('name', 'toko[' + bookIndex + '].jk').end();
-        })
-
-        // Remove button click handler
-        .on('click', '.removeButton', function() {
-            var $row  = $(this).parents('.form-group'),
-                index = $row.attr('index');
-
-            // Remove element containing the fields
-            $row.remove();
-        });
-        var urlw = "<?php echo URL; ?>";
-        var path = urlw + window.location.pathname.substring(1);
-        console.log(urlw);
-$('.nav>li>a[href="' + path + '"]').parent().addClass('active');
-
-});
-</script>
+<script src="<?php echo URL; ?>public/js/app.js"></script>
 </body>
 </html>
--- application/views/category/index.php
+++ application/views/category/index.php
@@ -11,7 +11,7 @@
                     <h3 class="panel-title">Tambah Kategori</h3>
                 </div>
                 <div class="panel-body">
-                    <form accept-charset="UTF-8" role="form">
+                    <form accept-charset="UTF-8" role="form" id="category_form">
                         <fieldset>
                             <div class="form-group">
                                 <label for="category_code" class="control-label">Kode kategori</label>
@@ -23,21 +23,35 @@
                                 <input class="form-control" placeholder="Nama kategori" name="category_name" type="text"
                                        required>
                             </div>
-                            <div class="form-group">
-                                <label class="control-label">Sub kategori 1:</label>
-                                <br/>
-                                <label for="subcategory_code" class="control-label">Kode subkategori</label>
-                                <input class="form-control" placeholder="Kode subkategori" name="subcategory_code"
-                                       type="text"
-                                       required>
+                            <div>
+                                <div class="form-group">
+                                    <label class="control-label">Sub kategori 1:</label>
+                                    <br/>
+                                    <label for="subcategory_code" class="control-label">Kode subkategori</label>
+                                    <input class="form-control" placeholder="Kode subkategori" name="subcategory_code[0]"
+                                           type="text" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="subcategory_name" class="control-label">Nama kategori</label>
+                                    <input class="form-control" placeholder="Nama subkategori" name="subcategory_name[0]"
+                                           type="text" required>
+                                </div>
                             </div>
-                            <div class="form-group">
-                                <label for="subcategory_name" class="control-label">Nama kategori</label>
-                                <input class="form-control" placeholder="Nama subkategori" name="subcategory_name"
-                                       type="text"
-                                       required>
+                            <div class="hide" id="subcategory_template">
+                                <div class="form-group">
+                                    <label class="control-label" id="label">Sub kategori 1:</label>
+                                    <br/>
+                                    <label for="subcategory_code" class="control-label">Kode subkategori</label>
+                                    <input class="form-control" placeholder="Kode subkategori" name="subcategory_code"
+                                           type="text" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="subcategory_name" class="control-label">Nama kategori</label>
+                                    <input class="form-control" placeholder="Nama subkategori" name="subcategory_name"
+                                           type="text" required>
+                                </div>
                             </div>
-                            <a class="btn btn-lg btn-success btn-block" href="#"
+                            <a class="btn btn-lg btn-success btn-block add-subcategory"
                                role="button">Tambah Subkategori</a>
                         </fieldset>
                     </form>
