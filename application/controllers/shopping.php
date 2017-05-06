ade3d5128c35f69c97bf8eeb3ee4fb5ba8a5cc42
Muhammad Kamal <muh.kmal@gmail.com>
2017-05-06 18:58:25 +0700

    add keranjang belanja

--- application/controllers/shopping.php
+++ application/controllers/shopping.php
@@ -17,6 +17,7 @@ class Shopping extends Controller
     public function listpulsa()
     {
         require APP . 'views/_templates/header.php';
+         require APP . 'views/_templates/customer_navbar.php';
         require APP . 'views/shopping/listpulsa.php';
         require APP . 'views/_templates/footer.php';
     }
@@ -24,14 +25,25 @@ class Shopping extends Controller
     public function listproduk()
     {
         require APP . 'views/_templates/header.php';
+         require APP . 'views/_templates/customer_navbar.php';
         require APP . 'views/shopping/listproduk.php';
         require APP . 'views/_templates/footer.php';
+        require APP . 'views/shopping/js.php';
     }
 
      public function shoppulsa()
     {
         require APP . 'views/_templates/header.php';
+         require APP . 'views/_templates/customer_navbar.php';
         require APP . 'views/shopping/shoppulsa.php';
+        require APP . 'views/_templates/footer.php';
+    }
+
+    public function cart()
+    {
+        require APP . 'views/_templates/header.php';
+         require APP . 'views/_templates/customer_navbar.php';
+        require APP . 'views/shopping/cart.php';
         require APP . 'views/_templates/footer.php';
     }
     
