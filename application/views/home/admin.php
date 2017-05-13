eccbcc9d5c5a990fe46f47ba73466ef0c636adeb
Zetra <zetra@mail.ugm.ac.id>
2017-05-13 14:29:40 +0700

    Adding admin home

---
+++ application/views/home/admin.php
@@ -0,0 +1,16 @@
+<?php if (!$this) {
+    exit(header('HTTP/1.0 403 Forbidden'));
+} ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="panel">
+                Selamat datang di tokokeren
+            </div>
+        </div>
+    </div>
+    <div class="row">
+
+    </div>
+</div>
