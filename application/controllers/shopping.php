ca029797e9ada04c8cd86a69cea8fc6e35daf73b
Muhammad Kamal <muh.kmal@gmail.com>
2017-05-20 22:38:01 +0700

    commit transction checkout

--- application/controllers/shopping.php
+++ application/controllers/shopping.php
@@ -35,13 +35,90 @@ class Shopping extends Controller
         header("location: " . URL . "index.php");
     }
 
-    public function listproduk()
+    public function addKeranjangBelanja()
     {
+        $kodeproduk = $_POST['kodeproduk'];
+        $berat = $_POST['berat'];
+       $kuantitas = $_POST['kuantitas'];
+       $harga = $_POST['harga'];
+ 
+        $transaksiresult = $this->model->addKeranjangBelanja($kodeproduk,$berat,$kuantitas,$_SESSION['user']->email,$harga);
+
+        header("location: " . URL . "index.php");
+    }
+
+     public function checkout()
+    {
+        
+        $alamat = $_POST['alamat'];
+       $jasakirim = $_POST['jasakirim'];
+
+       $listjk = $this->model-> getJasaKirimByNama($jasakirim);
+       $tarif=0;
+       $namatoko='';
+        foreach ($listjk as $jk) {
+          $tarif=$jk->tarif;
+          $namatoko=$jk->nama_toko;
+        }
+       
+       $totalberat = $_POST['totalberat'];
+       $subtotal = $_POST['subtotal'];
+       $biayakirim = $totalberat*$tarif;
+ //addTransaksiShipped($namatoko,$alamatkirim,$email,$biayakirim,$namajaskirim)
+        $transaksiresult = $this->model->addTransaksiShipped($namatoko,$alamat,$_SESSION['user']->email,$biayakirim,$jasakirim,$subtotal);
+
+        header("location: " . URL . "index.php");
+    }
+
+    
+
+     public function getsubkategori()
+    {
+        $katid = $_POST['katid'];
+         
+        $listsubkategori = $this->model->getSubKategori($katid);
+
+        //header("location: " . URL . "index.php");
+
+$obj = json_encode($listsubkategori, true);
+print_r($obj); // 'kamal' . $noinv;
+
+    }
+
+  public function getlistprodfilter()
+  {
+       $subkatid = $_POST['subkatid'];
+      $tokoid=$_POST['tokoid'];
+      $offset=0;
+        $listproduk = $this->model->getProdukShippedByKat($offset,str_replace('%20', ' ', $tokoid),$subkatid);
+$obj = json_encode($listproduk, true);
+print_r($obj); // 'kamal' . $noinv;
+    }
+
+
+    public function listproduk($tokoid,$offset)
+    {
+       $listproduk = $this->model->getProdukShipped($offset,str_replace('%20', ' ', $tokoid));
+       $listkategori =$this->model->getAllCategories();
+       $tokoid=str_replace('%20', ' ', $tokoid);
+ $totalProduct =  $this->model->getCountProdukShipped(str_replace('%20', ' ', $tokoid));
+
+       //echo str_replace('%20', ' ', $tokoid); 
         require APP . 'views/_templates/header.php';
          require APP . 'views/_templates/customer_navbar.php';
         require APP . 'views/shopping/listproduk.php';
         require APP . 'views/_templates/footer.php';
         require APP . 'views/shopping/js.php';
+    }
+
+    public function produktoko()
+    {
+       $listtoko = $this->model->getTokoAll();
+        require APP . 'views/_templates/header.php';
+         require APP . 'views/_templates/customer_navbar.php';
+        require APP . 'views/shopping/produktoko.php';
+        require APP . 'views/_templates/footer.php';
+        require APP . 'views/shopping/jstoko.php';
     }
 
      public function shoppulsa()
@@ -54,6 +131,10 @@ class Shopping extends Controller
 
     public function cart()
     {
+ $listcart = $this->model->getKeranjangBelanja($_SESSION['user']->email);
+$listJasaKirim = $this->model->getJasaKirimByToko($_SESSION['user']->email);
+$berattotal=0;
+$subtotal=0;
         require APP . 'views/_templates/header.php';
          require APP . 'views/_templates/customer_navbar.php';
         require APP . 'views/shopping/cart.php';
--- application/views/shopping/js.php
+++ application/views/shopping/js.php
@@ -4,17 +4,78 @@
 <script type="text/javascript">
     $(document).ready(function() {
     
+ $('#filterProduk').on('click', function() {
+     $('#tlistproduk tr').not(function(){ return !!$(this).has('th').length; }).remove();
+             $.ajax({  
+                type: "post",  
+                url: url + "/shopping/getlistprodfilter/",  
+                data: { subkatid: $('#subkat').val(), tokoid: $('#hftokoid').val() },  
+                datatype: "json",  
+                traditional: true,  
+                success: function (data) {  
+                     console.log(data);
+                      var object = $.parseJSON(data);
+                    //$("#tblmodal").empty();
 
-    $('#submitShowProduct').on('click', function() {
-             $('#listProduk').show();
-             $('#frmSelectToko').hide();
-        });
-
-    $('#submitCart').on('click', function() {
-        //this method will execute to entry to Keranjang belanja
-             
-             $('#modalAddCart').modal('toggle');
-        });
-                 
+                    
+                   // console.log(object);
+                     //$('#lblnoinvoice').text(result);
+                     var tbody = $("<tbody />"),tr;
+                      $.each(object, function (_,dprod) {
+                        //console.log(dprod["kode_produk"]);
+                          tr = $("<tr />");
+                          tr.append("<td>"+dprod["kode_produk"]+"</td>");
+                          tr.append("<td>"+dprod["nama"]+"</td>");
+                          tr.append("<td>"+dprod["harga"]+"</td>");
+                          tr.append("<td>"+dprod["deskripsi"]+"</td>");
+                          tr.append("<td>"+dprod["is_asuransi"]+"</td>");
+                          tr.append("<td>"+dprod["stok"]+"</td>");
+                          tr.append("<td>"+dprod["is_baru"]+"</td>");
+                          tr.append("<td>"+dprod["harga_grosir"]+"</td>");
+                          tr.append("<td><a href='"+url+"ulasan/mantap/"+dprod["kode_produk"]+"' class='btn btn-primary btn-sm'>Beli</a></td>");
+                          //$.each(obj,function(_,text) {
+                          //  tr.append("<td>"+text+"</td>")
+                          //});
+                          tr.appendTo(tbody);
+                      });
+                     tbody.appendTo("#tlistproduk");
+                }  
+            });  
+        });      
 });
-</script>
+
+    function loadFormBeli(kodeproduk,harga)
+    {
+        $('#kodeproduk').val(kodeproduk);
+
+        $('#harga').val(harga);
+
+        //$('#nominal').val(nominal);
+        $('#modalAddCart').modal('show');
+    }
+
+
+    $(document).ready(function () {  
+
+        $('#katUtama').change(function () {  
+            $.ajax({  
+                type: "post",  
+                url: url + "/shopping/getsubkategori/",  
+                data: { katid: $('#katUtama').val() },  
+                datatype: "json",  
+                traditional: true,  
+                success: function (data) {  
+                    $("#subkat").empty(); 
+                    //console.log(data);
+                    var object = $.parseJSON(data);
+                     $("#subkat").append('<option value="0">--Select--</option>');
+                    $.each(object, function (_,dsub) {
+                        //console.log(dprod["kode_produk"]);
+                        $("#subkat").append('<option value="'+dsub["kode"]+'">'+dsub["nama"]+'</option>');
+                      });
+                    
+                }  
+            });  
+        });  
+    });  
+</script> 
